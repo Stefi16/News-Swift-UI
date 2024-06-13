e3ddbd14c05b07df127330eb45eb189ca5e6bfe3
StefiK16 <144347897+StefiK16@users.noreply.github.com>
2024-06-13 17:38:46 +0300

    Initial Commit

---
+++ news/newsApp.swift
@@ -0,0 +1,17 @@
+//
+//  newsApp.swift
+//  news
+//
+//  Created by Stefka Krachunova on 13.06.24.
+//
+
+import SwiftUI
+
+@main
+struct newsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
